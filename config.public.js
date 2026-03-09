ba9f396e28c4b1c87bf11fa95e57f8af994d43c5
hsueh0706 <xxue0706@gmail.com>
2026-03-09 23:32:08 +0800

    切換到新的 GAS Admin URL

--- config.public.js
+++ config.public.js
@@ -5,7 +5,7 @@
 var FRONTEND_CONFIG = {
   API_URL: '',
   API_URL_PUBLIC:
-    'https://script.google.com/macros/s/AKfycbwoAz7sTthlp2cgsy6d0iX-wvC5v7OVA4ivPOckq2SOzt5TvUg773s-VmAofXTRF9YCyQ/exec',
+    'https://script.google.com/macros/s/AKfycbz6OYlohDVkAbjci8n4Uk0MCXsjM7V7R3q0GqNgWfIcDbcqjGEEJ7mtTIwrlJSH6ILw8w/exec',
   API_URL_ADMIN: '',
   ADMIN_API_KEY: '',
   RECAPTCHA_SITE_KEY: '6LdTR2wsAAAAAI9fy5CuyD42lZ6hGk4ed0bJbqIW',
