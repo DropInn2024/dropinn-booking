efd8de1532038c605241124748e0b9c4bf94eb7a
hsueh0706 <xxue0706@gmail.com>
2026-03-09 23:58:58 +0800

    set API_URL to new GAS endpoint

--- config.public.js
+++ config.public.js
@@ -3,12 +3,14 @@
  * 訂房首頁會先載入此檔，若本機有 config.js 則會覆蓋（本地開發／後台用）。
  */
 var FRONTEND_CONFIG = {
-  API_URL: '',
-  API_URL_PUBLIC:
-    'https://script.google.com/macros/s/AKfycbz6OYlohDVkAbjci8n4Uk0MCXsjM7V7R3q0GqNgWfIcDbcqjGEEJ7mtTIwrlJSH6ILw8w/exec',
-  API_URL_ADMIN: '',
-  ADMIN_API_KEY: '',
-  RECAPTCHA_SITE_KEY: '6LdTR2wsAAAAAI9fy5CuyD42lZ6hGk4ed0bJbqIW',
+  var FRONTEND_CONFIG = {
+    API_URL:
+      'https://script.google.com/macros/s/AKfycbz6OYlohDVkAbjci8n4Uk0MCXsjM7V7R3q0GqNgWfIcDbcqjGEEJ7mtTIwrlJSH6ILw8w/exec',
+    API_URL_PUBLIC:
+      'https://script.google.com/macros/s/AKfycbz6OYlohDVkAbjci8n4Uk0MCXsjM7V7R3q0GqNgWfIcDbcqjGEEJ7mtTIwrlJSH6ILw8w/exec',
+    API_URL_ADMIN: '',
+    ADMIN_API_KEY: '',
+    RECAPTCHA_SITE_KEY: '6LdTR2wsAAAAAI9fy5CuyD42lZ6hGk4ed0bJbqIW',
   BRAND_NAME: '雫旅 Drop Inn',
   BRAND_TAGLINE: '花火散落後，回到雫旅',
   CONTACT: {
